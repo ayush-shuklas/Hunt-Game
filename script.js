cc14ecb8e32443bddd218e8263d5d373e1a8bb62
ayush shukla <ayusshshuklaas@gmail.com>
2022-04-07 04:47:38 +0530

    File uploaded

---
+++ script.js
@@ -0,0 +1,7 @@
+// 'use strict';
+
+// document.querySelector('.check').addEventListener('click', function () {
+//     const gues = document.querySelector('.guess').value
+// console.log(gues)
+// });
+//     // console.log(gues);
